07f90e5e8f398e8e5745849e67141db2bf567f4c
DimitrivC <d.j.vancapelleveen@xs4all.nl>
2017-11-14 15:49:21 +0100

    niet heel veel

--- sample/helpers.py
+++ sample/helpers.py
@@ -106,3 +106,10 @@ class Graph:
 		            # add connection in list in both directions
 		            critical_connections.append([vid,wid,])
 				"""
+	#def hierholzer(self):
+	#a directed graph has an Eulerian cycle if following conditions are true (1) All vertices with nonzero degree belong to a single strongly connected component. (2) In degree and out degree of every vertex is same. The algorithm assumes that the given graph has Eulerian Circuit.
+
+#Choose any starting vertex v, and follow a trail of edges from that vertex until returning to v. It is not possible to get stuck at any vertex other than v, because indegree and outdegree of every vertex must be same, when the trail enters another vertex w there must be an unused edge leaving w.
+#The tour formed in this way is a closed tour, but may not cover all the vertices and edges of the initial graph.
+#As long as there exists a vertex u that belongs to the current tour but that has adjacent edges not part of the tour, start another trail from u, following unused edges until returning to u, and join the tour formed in this way to the previous tour.
+#Thus the idea is to keep following unused edges and removing them until we get stuck. Once we get stuck, we back-track to the nearest vertex in our current path that has unused edges, and we repeat the process until all the edges have been used. We can use another container to maintain the final path.
