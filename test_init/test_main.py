b23484b41118c4b1f9d1b3c4855de1c194106c74
Rick van Bork <rick-627@hotmail.com>
2017-11-21 13:08:08 +0100

    draw graph function added

--- test_init/test_main.py
+++ test_init/test_main.py
@@ -1,6 +1,7 @@
 import test_class
 import test_algos
 import test_helpers
+import analysis
 
 # initialize path files for Noord Holland graph
 path_stations_file = '../data/StationsHolland.csv'
@@ -14,9 +15,22 @@ hlp = test_helpers
 # make graph instance (Noord Holland)
 g = Graph("NH", path_stations_file, path_tracks_file)
 
-scores, p_scores, best_tracks = alg.random_walk(g, 1)
+scores, p_scores, best_tracks = alg.random_walk(g, 5)
+
+print(p_scores)
+
+count = 0
+for i in range(len(p_scores)):
+	if (p_scores[i] > 0.7) and (p_scores[i] < 0.8):
+		count += 1
+print(count)
 
 hlp.print_score_information(scores)
+
+# analysis.draw_graph(g)
+analysis.draw_p_barchart(p_scores)
+
+
 
 # print best tracks
 # for track in best_tracks:
