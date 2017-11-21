a0f2a34bcee6da22e463dbaeafa71df68a4a34f8
Rick van Bork <rick-627@hotmail.com>
2017-11-21 15:36:39 +0100

    import as added

--- test_init/test_main.py
+++ test_init/test_main.py
@@ -1,7 +1,7 @@
 import test_class
-import test_algos
-import test_helpers
-import analysis
+import test_algos as alg
+import test_helpers as hlp
+import analysis as ana
 
 # initialize path files for Noord Holland graph
 path_stations_file = '../data/StationsHolland.csv'
@@ -9,33 +9,17 @@ path_tracks_file = '../data/ConnectiesHolland.csv'
 
 # initialise graph class
 Graph = test_class.Graph
-alg = test_algos
-hlp = test_helpers
 
 # make graph instance (Noord Holland)
 g = Graph("NH", path_stations_file, path_tracks_file)
 
 scores, p_scores, best_tracks = alg.random_walk(g, 50)
 
-scores, p_scores, best_tracks = alg.random_walk(g, 1)
-
 # get score of smart random walk
 scores, p_scores, best_tracks = alg.smart_random_walk(g, 1)
 
 hlp.print_score_information(scores)
 
-analysis.draw_barchart(p_scores)
-
-#score_list = alg.hierholzer(g)
-
-# analysis.draw_graph(g)
-analysis.draw_p_barchart(p_scores)
+ana.draw_barchart(p_scores)
 
 score_list = alg.hierholzer(g)
-
-#print(score_list)
-
-# print best tracks
-# for track in best_tracks:
-# 	for key in track:
-# 		print("{}: {}".format(key, track[key]))
