5738e64c93fdb5436a174a3faa323643a4ce2d7f
thom <thom@laptop.com>
2017-11-21 12:54:50 +0100

    add smart randomish walk

--- test_init/test_algos.py
+++ test_init/test_algos.py
@@ -119,3 +119,130 @@ def random_walk(graph, iterator):
 		print("Score: {}".format(score))
 
 	return s_list, p_list, best_tracks
+
+
+
+'''
+Smart 'random' walk
+'''
+
+def random_walk(graph, iterator):
+
+	# get information from graph to perform algorithm
+	nodelist = graph.nodes
+	minimum_weight = graph.minimal_edge_weight
+	critical_connections = graph.critical_edge_list
+	total_critical_connections = graph.total_critical_edges
+	G = graph.G
+	hlp = test_helpers
+
+	# set lists
+	p_list = []
+	s_list = []
+	best_tracks = []
+	best_score = - 141
+
+	# do the walk iterator amount of times
+	for i in range(iterator):
+
+		# start a list of unique critical tracks the random walk traverses
+		critical_connections_traversed = []
+
+		# begin new track dict
+		all_connections = {"tracks": {}, "score": None}
+
+		# rand number of tracks 1 up to including 7
+		random_tracks = 7
+
+		print("Track number: {}".format(random_tracks))
+
+		# keep track of critical connections that are not used yet
+		total_time = 0
+
+		# builds the service of multiple tracks
+		for track in range(random_tracks):
+
+			# begin new dict with lists
+			all_connections["tracks"][str(track)] = []
+
+			# rand start station 0 up to nodelist length - 1 to pick a node in nodelist
+			# take a random critical station as starting station instead of a total random station
+			starting_station = random.choice(graph.critical_station_list)
+
+			print("Begin walk: {}".format(starting_station))
+
+			# keeps track of the time of one track of a service
+			track_time = 0
+
+			# Use maximum time for each train (as opposed to random time)
+			random_time = 120
+
+			print("Track maximum time: {}".format(random_time))
+
+			counter = 0
+			while track_time < random_time:
+				
+				# get list of critical neighbours of starting_station
+				critical_neighbors = [station for station in list(graph.G[starting_station]) if station in graph.critical_station_list]
+				
+
+				if critical_neighbors == []:
+					
+				random_neighbor = random.choice(list(G[starting_station]))
+
+				# keeps track of time of the track
+				edge_time = G[starting_station][random_neighbor]['weight']
+				track_time += edge_time
+
+				# append each new edge to track
+				all_connections["tracks"][str(track)].append((starting_station, random_neighbor))
+				
+				# always pick one track
+				if ((edge_time > random_time) and counter != 0) or (edge_time > random_time - track_time):
+					print("		Exception!")
+					break
+				counter += 1
+
+				# update traversed critical connections as long as not all have been covered
+				if len(critical_connections_traversed) != total_critical_connections:
+					critical_connections_traversed = hlp.update_critical_connections_travesed((starting_station, random_neighbor), critical_connections, critical_connections_traversed)
+				else:
+					print("`GOT EM!") # yeah right...
+
+				# updates the starting station
+				starting_station = random_neighbor
+				print("		Next station: {}".format(random_neighbor))
+				print("			Edge time is: {}".format(edge_time))
+				print("			Total time is: {}".format(track_time))
+				print("-------------------------------------------------")
+
+			# total time of the service
+			total_time = track_time
+
+			print("This service will take {}min\n".format(track_time))
+		
+		# percentage of critical tracs traversed
+		p = hlp.get_p(critical_connections_traversed, critical_connections)
+		p_list.append(p)
+
+		# append score
+		s = hlp.get_score(p, random_tracks, total_time)
+		s_list.append(s)
+
+		# update best track if its score is better than previous best
+		if s > best_score:
+			best_tracks.append(all_connections)
+			best_score = s
+
+		all_connections["score"] = s
+
+		print("Connections made:")
+		for i in range(random_tracks):
+			print("Track {}:	".format(i + 1))
+			edgeList = all_connections["tracks"][str(i)]
+			score = all_connections["score"]
+			for edge in edgeList:
+				print("		{}".format(edge))
+		print("Score: {}".format(score))
+
+	return s_list, p_list, best_tracks
