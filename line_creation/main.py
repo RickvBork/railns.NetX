0d3012a0028ec6177cc1752e4e4bf8d7813199db
thom <thom@laptop.com>
2017-11-23 09:50:37 +0100

    add csv writer to main

--- line_creation/main.py
+++ line_creation/main.py
@@ -2,6 +2,7 @@ import line_algorithms as alg
 import line_analysis as ana
 import line_graph_class
 import helpers as hlp
+import csv
 
 # initialize path files for Noord Holland graph
 path_stations_file = '../data/StationsHolland.csv'
@@ -39,5 +40,13 @@ print(p_scores)
 # 		print("{}: {}".format(key, track[key]))
 
 ana.draw_barchart(p_scores)
+
+
+# write scores to csv
+# ref: https://stackoverflow.com/questions/39282516/python-list-to-csv-throws-error-iterable-expected-not-numpy-int64
+# ref: https://docs.python.org/3/library/csv.html
+with open('../data/results.csv', 'w', newline='') as myfile:
+	wr = csv.writer(myfile,delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+wr.writerows(map(lambda x: [x], scores))
 
 
