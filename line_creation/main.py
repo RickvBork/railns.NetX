4b904ee35163b3b0f844c21d84ff3e403608ef31
thom <thom@laptop.com>
2017-11-23 10:50:42 +0100

    feedback week 4

--- line_creation/main.py
+++ line_creation/main.py
@@ -3,6 +3,8 @@ import line_analysis as ana
 import line_graph_class
 import helpers as hlp
 import csv
+from networkx.linalg.graphmatrix import adjacency_matrix
+import scipy
 
 # initialize path files for Noord Holland graph
 path_stations_file = '../data/StationsHolland.csv'
@@ -41,12 +43,14 @@ print(p_scores)
 
 ana.draw_barchart(p_scores)
 
+print(adjacency_matrix(g.G))
+
 
 # write scores to csv
 # ref: https://stackoverflow.com/questions/39282516/python-list-to-csv-throws-error-iterable-expected-not-numpy-int64
 # ref: https://docs.python.org/3/library/csv.html
-with open('../data/results.csv', 'w', newline='') as myfile:
+'''with open('../data/results.csv', 'w', newline='') as myfile:
 	wr = csv.writer(myfile,delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	wr.writerows(map(lambda x: [x], scores))
+	wr.writerows(map(lambda x: [x], scores))'''
 
 
